58856bbb730c4e9e6c7bb2c9b09dcb68b56ecc28
RBozhilova <rdbozhilova@gmail.com>
2022-08-25 11:43:19 +0300

    Django project removed

--- django-project/songs/pesni4ki/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('pesen/<int:pk>', views.SongDetailView.as_view(), name='song-detail'),
-]
