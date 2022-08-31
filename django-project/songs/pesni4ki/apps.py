b51305ab6777eac6b4de82f6d61f4ca5acf6d4c1
RBozhilova <rdbozhilova@gmail.com>
2022-08-31 16:32:18 +0300

    remove django project

--- django-project/songs/pesni4ki/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class Pesni4KiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pesni4ki'
-    verbose_name = "Каталог на песни"
